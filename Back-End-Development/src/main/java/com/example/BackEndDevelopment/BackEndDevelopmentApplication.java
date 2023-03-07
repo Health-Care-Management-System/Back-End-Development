d6c03812207ccc84ea1b4caf88db56a9df9d440e
Premanayake M.M.D.D <dinirangapremanayake@gmail.com>
2023-03-07 20:49:54 +0530

    Initial Files

---
+++ Back-End-Development/src/main/java/com/example/BackEndDevelopment/BackEndDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackEndDevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndDevelopmentApplication.class, args);
+	}
+
+}
